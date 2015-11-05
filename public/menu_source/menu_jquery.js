098c25da7b1b6a48eee163fc349316e44702b429
thouhtfull solutions <panic@bilba.fluff.com>
2015-11-05 03:16:16 +0000

    menu

--- public/menu_source/menu_jquery.js
+++ public/menu_source/menu_jquery.js
@@ -7,20 +7,10 @@ $('#cssmenu > ul > li > a').click(function() {
     $(this).closest('li').removeClass('active');
     checkElement.slideUp('normal');
   }
-  
-	('#cssmenu li').on('click', function(e){
-		var $target = $(e.target),
-        $currentElement = $(this);
-        
-		if (!$target.hasClass('sub-link') && !$currentElement.hasClass('active')){
-			var $activeElement = $('#cssmenu li.active');
-			$activeElement.find('ul').slideUp('normal');
-			$activeElement.removeClass('active');
-			
-			$currentElement.addClass('active');
-			$currentElement.find('ul').slideDown('normal');
-		}
-	}
+  if((checkElement.is('ul')) && (!checkElement.is(':visible'))) {
+    $('#cssmenu ul ul:visible').slideUp('normal');
+    checkElement.slideDown('normal');
+  }
   if($(this).closest('li').find('ul').children().length == 0) {
     return true;
   } else {
